dfbd1e4f6d8c021a8d42d4dfd234b4584b89018d
Hugo Trippaers <hugo@trippaers.nl>
2014-07-15 15:52:33 +0200

    Add a simple loop to test ssh

--- systemvmtests/create_systemvm.sh
+++ systemvmtests/create_systemvm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VBOXMNG="/usr/bin/VBoxManage"
+ROUTERIP=192.168.56.30
 
 $VBOXMNG createvm --name "cs-systemvm" \
                   --ostype "Ubuntu_64" \
@@ -15,7 +16,7 @@ $VBOXMNG storageattach cs-systemvm \
                   --storagectl "ctl0" \
                   --port 0 \
                   --type hdd \
-                  --medium /Users/hugo/VirtualBox\ VMs/templates/systemvm64template-unknown-xen-vpctoolkit.vhd \
+                  --medium /Users/hugo/VirtualBox\ VMs/templates/systemvm64template-vpc-toolkit-hugo-xen.vhd \
                   --mtype multiattach
 
 $VBOXMNG storageattach cs-systemvm \
@@ -26,7 +27,23 @@ $VBOXMNG storageattach cs-systemvm \
 
 $VBOXMNG modifyvm cs-systemvm \
                   --nic1 hostonly \
-                  --hostonlyadapter1 "vboxnet0"
+                  --hostonlyadapter1 "vboxnet0" \
+                  --memory 256
 
 $VBOXMNG setextradata cs-systemvm \
-                  "VBoxInternal/Devices/pcbios/0/Config/DmiOEMVBoxRev" "cmdline:console=hvc0 vpccidr=172.16.0.0/16 domain=devcloud.local dns1=8.8.8.8 dns2=8.8.8.4 template=domP name=r-8-VM eth0ip=192.168.56.30 eth0mask=255.255.255.0 type=vpcrouter disable_rp_filter=true"
+                  "VBoxInternal/Devices/pcbios/0/Config/DmiOEMVBoxRev" "cmdline:console=hvc0 vpccidr=172.16.0.0/16 domain=devcloud.local dns1=8.8.8.8 dns2=8.8.8.4 template=domP name=r-8-VM eth0ip=$ROUTERIP eth0mask=255.255.255.0 type=vpcrouter disable_rp_filter=true"
+
+$VBOXMNG startvm cs-systemvm
+
+while ! ssh -p 3922 -o ConnectTimeout=2 root@${ROUTERIP} /opt/cloud/bin/get_template_version.sh | grep -q -v CloudStack ; do
+  sleep 3
+done
+
+echo executing get_template_version.sh
+ssh -p 3922 root@${ROUTERIP} /opt/cloud/bin/get_template_version.sh
+
+$VBOXMNG controlvm cs-systemvm poweroff
+
+sleep 2 
+
+$VBOXMNG unregistervm cs-systemvm --delete
